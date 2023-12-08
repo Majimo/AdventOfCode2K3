2f6aca12fd9f90cc4f6ea7560d4d7b70c560d0d6
Pierre Fervel <pierre.fervel@gmail.com>
2023-12-08 15:11:42 +0100

    Day 8 Part 1, Test Ok, Puzzle KO

--- day8/main.py
+++ day8/main.py
@@ -1,34 +1,52 @@
+from operator import ne
 
 
+def check_if_next_step_is_ZZZ(next_step, instructions, directions, r = 0):
+    r += 1
+    print('RESET .....', r, next_step)
+    directions = directions + directions 
+    steps = 0
+    next_step = 'AAA'
+    for direction in directions:
+        next_step, steps = navigate_through_map(direction, instructions, next_step, steps)
+        if next_step == 'ZZZ':
+            break
 
-def navigate_through_map(direction, instructions, count):
-    print(instructions)
-    start_index = instructions[0].find('(')
-    end_index = instructions[0].find(')')
+    if next_step != 'ZZZ':
+        next_step = (instructions[0].split(' ='))[0]
+        check_if_next_step_is_ZZZ(next_step, instructions, directions, r)
+    
+    return steps
 
-    content = instructions[0][start_index + 1:end_index]
+
+def navigate_through_map(direction, instructions, next_step, count):
+    i = next((index for index, ligne in enumerate(instructions) if next_step + ' =' in ligne), None)
+    start_index = instructions[i].find('(')
+    end_index = instructions[i].find(')')
+
+    content = instructions[i][start_index + 1:end_index]
     values = [value.strip() for value in content.split(',')]
     result = values[1] if direction == 'R' else values[0]
 
-    print(result)
     count += 1
     return result, count
 
 with open("input.txt") as f:
     lines = [line.strip() for line in f.readlines()]
     directions = [char for char in lines[0]]
-    instructions = [line for line in lines[1:]]
+    instructions = [line for line in lines[2:]]
 
     steps = 0
-    next_step = 'AAA'
-    for i, direction in enumerate(directions):
-        next_map = [line for line in instructions[(i + 1):] if next_step in line]
-        if len(next_map) == 0:
-            next_map = [line for line in instructions if next_step in line]
-        print(next_map)
-        next_step, steps = navigate_through_map(direction, next_map, steps)
-        if len(next_step) == 0:
-            print('oh non !')
+    next_step = (instructions[0].split(' ='))[0]
+
+    for direction in directions:
+        next_step, steps = navigate_through_map(direction, instructions, next_step, steps)
+        if next_step == 'ZZZ':
+            break
+    
+    if next_step != 'ZZZ':
+        next_step = (instructions[0].split(' ='))[0]
+        steps = check_if_next_step_is_ZZZ(next_step, instructions, directions)
 
     print('PART 1 :', steps)
 
